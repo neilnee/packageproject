514527f6cf9c7bd9811b1d597eac4eb772437669
Neil Nee <neilnee324@gmail.com>
2014-06-11 16:19:55 +0800

    commit some change

---
+++ MavenLibPack/src/com/neil/mavenlibpack/BasicLib.java
@@ -0,0 +1,9 @@
+package com.neil.mavenlibpack;
+
+public class BasicLib {
+
+    public static String createString() {
+        return "I am String";
+    }
+    
+}
--- MavenPackage/src/com/neil/mavenpackage/MainActivity.java
+++ MavenPackage/src/com/neil/mavenpackage/MainActivity.java
@@ -2,13 +2,25 @@ package com.neil.mavenpackage;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.neil.mavenlibpack.BasicLib;
 
 public class MainActivity extends Activity {
 
+    private TextView mTextView = null;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initControllers();
+    }
+    
+    private final void initControllers() {
+        mTextView = (TextView) findViewById(R.id.textview);
+        mTextView.setText(BasicLib.createString());
     }
 
 }
