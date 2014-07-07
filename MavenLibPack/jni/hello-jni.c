97b341b8461d66752b61f95185db1d14b55f0fdd
Neil Nee <neilnee324@gmail.com>
2014-07-07 17:53:57 +0800

    增加meta-data的使用

--- MavenLibPack/jni/hello-jni.c
+++ MavenLibPack/jni/hello-jni.c
@@ -5,5 +5,5 @@ jstring
 Java_com_neil_mavenlibpack_NativeLib_stringFromJNI( JNIEnv* env,
                                                   jobject thiz )
 {
-    return (*env)->NewStringUTF(env, "Hello from JNI!!!!");
+    return (*env)->NewStringUTF(env, "Hello from JNI! HeHe");
 }
--- MavenPackage/src/com/neil/mavenpackage/MainActivity.java
+++ MavenPackage/src/com/neil/mavenpackage/MainActivity.java
@@ -1,6 +1,11 @@
 package com.neil.mavenpackage;
 
 import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.pdf.PdfDocument.PageInfo;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -9,23 +14,33 @@ import com.neil.mavenlibpack.NativeLib;
 
 public class MainActivity extends Activity {
 
-    private TextView mTextViewLib = null;
-    private TextView mTextViewNative = null;
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        initControllers();
-    }
-    
-    private final void initControllers() {
-    	mTextViewLib = (TextView) findViewById(R.id.textview_lib);
-    	mTextViewNative = (TextView) findViewById(R.id.textview_native);
-    	
-    	mTextViewLib.setText(BasicLib.createString());
-    	mTextViewNative.setText(NativeLib.createNativeString());
-    }
+	private String mChannelID = null;
+	private TextView mTextViewLib = null;
+	private TextView mTextViewNative = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		try {
+			ApplicationInfo info = getPackageManager().getApplicationInfo(
+					getPackageName(), PackageManager.GET_META_DATA);
+			mChannelID = info.metaData.getString("channel_id");
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		initControllers();
+	}
+
+	private final void initControllers() {
+		mTextViewLib = (TextView) findViewById(R.id.textview_lib);
+		mTextViewNative = (TextView) findViewById(R.id.textview_native);
+
+		mTextViewLib.setText("BasicLib say : " + BasicLib.createString()
+				+ " , NativeLib say : " + NativeLib.createNativeString());
+		mTextViewNative.setText("mChannelID : " + mChannelID);
+	}
 
 }
